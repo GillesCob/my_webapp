249abf38594e9fa4d6c8f73e442228383bea2ac3
Gilles <104164728+GillesCob@users.noreply.github.com>
2023-11-24 11:11:23 +0100

    V1

---
+++ src/webapp/views.py
@@ -0,0 +1,31 @@
+from flask import render_template, Blueprint
+from flask_login import current_user, login_required
+from website.models import User
+
+views = Blueprint("views", __name__)
+
+
+@views.route('/')
+def home_page():
+    return render_template('home.html',user=current_user)
+
+@views.route('/menu-1')
+def menu_1():
+    return render_template('menu_1.html',user=current_user)
+
+@views.route('/menu-2')
+@login_required
+def menu_2():
+    return render_template('menu_2.html',user=current_user)
+
+@views.route('/login')
+def login():
+    return render_template('login.html',user=current_user)
+
+@views.route('/sign-up')
+def sign_up():
+    return render_template('sign_up.html',user=current_user)
+
+@views.route('/log-out')
+def log_out():
+    return render_template('log_out.html',user=current_user)
